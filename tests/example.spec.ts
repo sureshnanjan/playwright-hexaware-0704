dfc7d4425d10d034d88f6efe0b813beee0222af3
sureshnanjan <sureshnanjan@outlook.com>
2024-07-17 17:58:17 +0530

    Framework Discussions and Doc Comments Related

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { HomePage } from '../heroku_operations/home_page';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
@@ -11,8 +12,17 @@ test('get started link', async ({ page }) => {
   await page.goto('/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'abtest' }).click();
+  //await page.getByRole('link', { name: 'abtest' }).click();
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading')).toHaveText("Welcome to the-internet");
+});
+
+test('Check Title', async ({ page }) => {
+  const app = new HomePage()
+  const expected_title = "Welcome to the-internet"
+  const actual_title = app.getTitle()
+  //app.getAllAvailableExamples(1,)
+  await expect(actual_title).toEqual(expected_title)
+  
 });
